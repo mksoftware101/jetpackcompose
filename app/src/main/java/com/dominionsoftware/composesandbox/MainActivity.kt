b65fe2671b676f047968be4fceeaa111c5046fdd
Marek Kaszycki <marek.kaszycki@northmill.se>
2022-08-23 08:43:20 +0200

    Added DrinkWaterCounter composable

--- app/src/main/java/com/dominionsoftware/composesandbox/MainActivity.kt
+++ app/src/main/java/com/dominionsoftware/composesandbox/MainActivity.kt
@@ -3,41 +3,13 @@ package com.dominionsoftware.composesandbox
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.dominionsoftware.composesandbox.ui.theme.ComposeSandboxTheme
+import com.dominionsoftware.composesandbox.drinkwatercounter.DrinkWaterCounterMainScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeSandboxTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            DrinkWaterCounterMainScreen()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeSandboxTheme {
-        Greeting("Android")
     }
 }
