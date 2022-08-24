bfcdd5df8bb8d2e645c2aadd6ab858614f2d3854
Marek Kaszycki <marek.kaszycki@northmill.se>
2022-08-24 16:17:07 +0200

    AlignYourBodyElement compose

--- app/src/main/java/com/dominionsoftware/composesandbox/codelab/basiclayouts/MySootheMainActivity.kt
+++ app/src/main/java/com/dominionsoftware/composesandbox/codelab/basiclayouts/MySootheMainActivity.kt
@@ -3,21 +3,23 @@ package com.dominionsoftware.composesandbox.codelab.basiclayouts
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.heightIn
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.SnackbarDefaults.backgroundColor
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.material.TextFieldDefaults
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -65,4 +67,32 @@ class MySootheMainActivity : ComponentActivity() {
         }
     }
 
+    @Composable
+    fun AlignYourBodyElement(
+        @DrawableRes drawable: Int,
+        @StringRes text: Int,
+        modifier: Modifier
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = modifier) {
+            Image(
+                painterResource(id = drawable),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(88.dp)
+                    .clip(CircleShape)
+            )
+            Text(
+                stringResource(id = text),
+                style = MaterialTheme.typography.h3,
+                modifier = Modifier.paddingFromBaseline(top = 24.dp, bottom = 8.dp)
+            )
+        }
+    }
+
+    @Composable
+    @Preview
+    fun AlignYourBodyElementPreview() {
+        AlignYourBodyElement(drawable = R.drawable.ab1_inversions, text = R.string.ab1_inversions, modifier = Modifier)
+    }
 }
