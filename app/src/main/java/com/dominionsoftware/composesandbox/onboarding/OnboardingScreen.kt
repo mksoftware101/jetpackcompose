7fe2d9114ef345968b9966d88aff5f2e8bf00129
Marek Kaszycki <marek.kaszycki@northmill.se>
2022-08-26 11:58:20 +0200

    Extract OnboardingComponent

--- app/src/main/java/com/dominionsoftware/composesandbox/onboarding/OnboardingScreen.kt
+++ app/src/main/java/com/dominionsoftware/composesandbox/onboarding/OnboardingScreen.kt
@@ -1,7 +1,13 @@
 package com.dominionsoftware.composesandbox.onboarding
 
-import androidx.compose.foundation.*
+import androidx.annotation.ColorRes
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
@@ -9,7 +15,6 @@ import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
@@ -25,17 +30,33 @@ import com.dominionsoftware.composesandbox.ui.theme.ComposeSandboxTheme
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 @Composable
-fun OnboardingScreen(modifier: Modifier = Modifier) {
-    val systemUiController = rememberSystemUiController()
-    systemUiController.setSystemBarsColor(
-        color = colorResource(id = R.color.color1),
-        darkIcons = false
+fun OnboardingScreen() {
+    OnboardingComponent(
+        title = R.string.onboardingFirstScreenTitle,
+        text = R.string.onboardingFirstScreenText,
+        picture = R.drawable.onboarding_pic1,
+        backgroundColor = R.color.color1,
+        textColor = R.color.white,
+        isLastScreen = false
     )
+}
+
+@Composable
+fun OnboardingComponent(
+    @StringRes title: Int,
+    @StringRes text: Int,
+    @DrawableRes picture: Int,
+    @ColorRes backgroundColor: Int,
+    @ColorRes textColor: Int,
+    isLastScreen: Boolean,
+    modifier: Modifier = Modifier
+) {
+    SystemBarsColor(backgroundColor)
 
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(colorResource(id = R.color.color1))
+            .background(colorResource(id = backgroundColor))
     ) {
         Column(
             modifier = Modifier
@@ -45,7 +66,7 @@ fun OnboardingScreen(modifier: Modifier = Modifier) {
             Spacer(Modifier.height(46.dp))
             Row {
                 Image(
-                    painter = painterResource(id = R.drawable.onboarding_pic1),
+                    painter = painterResource(id = picture),
                     contentDescription = null,
                     modifier = Modifier.weight(1f),
                     contentScale = ContentScale.FillWidth
@@ -54,8 +75,8 @@ fun OnboardingScreen(modifier: Modifier = Modifier) {
             Spacer(modifier = Modifier.height(36.dp))
             Row(horizontalArrangement = Arrangement.Center) {
                 Text(
-                    text = stringResource(id = R.string.onboardingFirstScreenTitle),
-                    color = colorResource(id = R.color.white),
+                    text = stringResource(id = title),
+                    color = colorResource(id = textColor),
                     style = MaterialTheme.typography.h4,
                     letterSpacing = 1.5.sp,
                     fontWeight = FontWeight.Bold,
@@ -69,8 +90,8 @@ fun OnboardingScreen(modifier: Modifier = Modifier) {
                 modifier = Modifier.padding(start = 32.dp, end = 32.dp)
             ) {
                 Text(
-                    text = stringResource(id = R.string.onboardingFirstScreenText),
-                    color = colorResource(id = R.color.white),
+                    text = stringResource(id = text),
+                    color = colorResource(id = textColor),
                     textAlign = TextAlign.Center,
                     style = MaterialTheme.typography.h6.copy(lineHeight = 24.sp),
                     fontWeight = FontWeight.Light,
@@ -82,29 +103,51 @@ fun OnboardingScreen(modifier: Modifier = Modifier) {
             verticalAlignment = Alignment.CenterVertically,
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(start= 32.dp, end = 32.dp, top=32.dp, bottom = 20.dp)
+                .padding(start = 32.dp, end = 32.dp, top = 32.dp, bottom = 20.dp)
         ) {
-            Text(
-                text = stringResource(id = R.string.onboardingButtonSkip),
-                color = colorResource(id = R.color.white),
-                fontWeight = FontWeight.Light,
-            )
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                Text(
-                    text = stringResource(id = R.string.onboardingButtonNext),
-                    fontWeight = FontWeight.Bold,
-                    color = colorResource(id = R.color.white)
-                )
-                Spacer(modifier = Modifier.padding(4.dp))
-                Image(
-                    imageVector = Icons.Default.ArrowForward,
-                    contentDescription = "Next",
-                    colorFilter = ColorFilter.tint(colorResource(id = R.color.white))
-                )
+            if (isLastScreen) {
+                GetStartedButton()
+            } else {
+                OnboardingNavigationButtons(textColor)
             }
-
         }
     }
+}
+
+@Composable
+private fun OnboardingNavigationButtons(textColor: Int) {
+    Text(
+        text = stringResource(id = R.string.onboardingButtonSkip),
+        color = colorResource(id = textColor),
+        fontWeight = FontWeight.Light,
+    )
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Text(
+            text = stringResource(id = R.string.onboardingButtonNext),
+            fontWeight = FontWeight.Bold,
+            color = colorResource(id = textColor)
+        )
+        Spacer(modifier = Modifier.padding(4.dp))
+        Image(
+            imageVector = Icons.Default.ArrowForward,
+            contentDescription = "Next",
+            colorFilter = ColorFilter.tint(colorResource(id = textColor))
+        )
+    }
+}
+
+@Composable
+fun GetStartedButton() {
+    TODO("Not yet implemented")
+}
+
+@Composable
+private fun SystemBarsColor(backgroundColor: Int) {
+    val systemUiController = rememberSystemUiController()
+    systemUiController.setSystemBarsColor(
+        color = colorResource(id = backgroundColor),
+        darkIcons = false
+    )
 }
 
 @Preview(widthDp = 360, heightDp = 722)
